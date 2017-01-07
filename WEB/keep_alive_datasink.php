ac0279aac4eb7d19e1e0b8cf16e0a000738425d1
Slartitorto <ivano@98:0c:82:ff:4e:96.homenet.telecomitalia.it>
2017-01-07 18:10:59 +0100

    Reset alarmed flag every time

--- WEB/keep_alive_datasink.php
+++ WEB/keep_alive_datasink.php
@@ -10,6 +10,7 @@ if(isset($_GET['router'])){
 
 $result = mysqli_query($conn,"delete from keep_alive_check where router = '$router'");
 $result = mysqli_query($conn,"insert into keep_alive_check (router) values ('$router')");
+$result = mysqli_query($conn,"update keep_alive_check set alarmed = 0 where router = '$router'");
 
 mysqli_close($conn);
 ?>
